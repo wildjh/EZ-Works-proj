0744f2dedbabfd574f14749795f687a3408a5d1c
Darpi <iangmartinezt@gmail.com>
2026-05-27 22:58:16 -0500

    añadido import.sql para creación automatica de roles

---
+++ ezworks-backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT IGNORE INTO rol (id, codigo, nombre) VALUES (1, 'ADMIN', 'Administrador');
+INSERT IGNORE INTO rol (id, codigo, nombre) VALUES (2, 'AYUDANTE', 'Ayudante');
+INSERT IGNORE INTO rol (id, codigo, nombre) VALUES (3, 'EMPLEADOR', 'Empleador');
