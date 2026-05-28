64341619b76c4cdf234d497c11d947e2c8d2623a
Darpi <iangmartinezt@gmail.com>
2026-05-27 20:45:33 -0500

    chore: ignorar carpeta target de ezworks-backend

--- ezworks-backend/target/classes/db/migration/V2__seed_categorias.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO categoria (nombre, activa) VALUES
-    ('Mandados y compras', TRUE),
-    ('Limpieza', TRUE),
-    ('Mascotas', TRUE),
-    ('Reparaciones menores', TRUE),
-    ('Jardinería', TRUE);
